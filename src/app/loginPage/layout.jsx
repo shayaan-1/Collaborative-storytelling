a75da15287157b421ec3d000238357e2e95fb002
shayaan-1 <shayanbutt561@gmail.com>
2025-05-18 12:31:51 -0700

    work on login started

---
+++ src/app/loginPage/layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div>
+        <div>layout</div>
+        <div>{children}</div>
+    </div>
+    
+  )
+}
+
+export default layout
---
+++ src/app/loginPage/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>loginPafe</div>
+  )
+}
+
+export default page
